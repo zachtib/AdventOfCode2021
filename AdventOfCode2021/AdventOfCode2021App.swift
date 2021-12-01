0f2b8ebd4bca143bd86c85fb1d63fd81591415e7
Zach Tibbitts <ztibbitts@etsy.com>
2021-12-01 11:13:11 -0500

    Initial Commit

---
+++ AdventOfCode2021/AdventOfCode2021App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2021App.swift
+//  AdventOfCode2021
+//
+//  Created by Zachary Tibbitts on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
